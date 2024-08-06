9d69e5e64cb5ecde7a8d3e5725103b71e7ee8973
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-08-06 14:35:36 +0900

    fix Sum.test.tsx

--- app/component/Sum.test.tsx
+++ app/component/Sum.test.tsx
@@ -1,32 +1,56 @@
-import { render, screen } from '@testing-library/react';
-import { expect, test } from 'vitest';
-import Sum from './Sum';
-import { Schema } from '@/amplify/data/resource';
+import { Schema } from "@/amplify/data/resource";
+import { render, screen } from "@testing-library/react";
+import { beforeAll, expect, test } from "vitest";
+import Sum from "./Sum";
 
-test('Sum', () => {
-    const details = [
-        {
-            id: '1',
-            name: 'Test Detail 1',
-            price: 1000,
-            label: 'Test Label 1',
-            paidByUserA: true,
-            paidByUserB: false,
-            paidAt: '2022-01-01',
-        },
-        {
-            id: '2',
-            name: 'Test Detail 2',
-            price: 2000,
-            label: 'Test Label 2',
-            paidByUserA: false,
-            paidByUserB: true,
-            paidAt: '2022-02-01',
-        },
-        // 他のテスト用データ...
-    ];
-    render(<Sum labeledDetails={details as Schema["Detail"]["type"][]} />)
+beforeAll(() => {
+  process.env.NEXT_PUBLIC_USER_A = "User A";
+  process.env.NEXT_PUBLIC_USER_B = "User B";
+});
 
-    const price = "￥500";
-    expect(screen.getByText(price)).toBeDefined();
-})
+test("Sum", () => {
+  const details: Schema["Detail"]["type"][] = [
+    {
+      id: "1",
+      name: "Test Detail 1",
+      price: 1000,
+      label: "Test Label 1",
+      paidByUserA: true,
+      paidByUserB: false,
+      paidAt: "2022-01-01",
+      createdAt: "2000-01-01",
+      updatedAt: "2000-01-01",
+    },
+    {
+      id: "2",
+      name: "Test Detail 2",
+      price: 2000,
+      label: "Test Label 2",
+      paidByUserA: false,
+      paidByUserB: true,
+      paidAt: "2022-02-01",
+      createdAt: "2000-01-01",
+      updatedAt: "2000-01-01",
+    },
+    {
+      id: "3",
+      name: "Test Detail 3",
+      price: 2500,
+      label: "Test Label 3",
+      paidByUserA: false,
+      paidByUserB: true,
+      paidAt: "2022-02-01",
+      createdAt: "2000-01-01",
+      updatedAt: "2000-01-01",
+    },
+  ];
+  render(<Sum labeledDetails={details as Schema["Detail"]["type"][]} />);
+
+  const actualSumA =
+    screen.getByText("User A - 未払い差引合計").nextSibling?.textContent;
+  expect(actualSumA).toBe("￥1,750");
+
+  const actualSumB =
+    screen.getByText("User B - 未払い差引合計").nextSibling?.textContent;
+  expect(actualSumB).toBe("￥0");
+});
