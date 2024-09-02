a9f83b392a600d574874723c5b4befde203d1fd8
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-09-02 09:33:47 +0900

    organize imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import AppVersion from "./component/AppVersion";
 import Data from "./component/Data";
 import Header from "./component/Header";
-import LoginUserInfo from "./component/LoginUserInfo";
 
 const Home: React.FC = () => {
   return (
