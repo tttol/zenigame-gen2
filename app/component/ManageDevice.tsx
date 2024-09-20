dc85b0704a2b961d14357aed448d238890167ce7
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-09-20 21:37:30 +0900

    finish create credential logic

--- app/component/ManageDevice.tsx
+++ app/component/ManageDevice.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { StoredCredential } from "amazon-cognito-passwordless-auth/fido2";
 import { usePasswordless } from "amazon-cognito-passwordless-auth/react";
 import React, { useState } from "react";
 import Loading from "./Loading";
@@ -17,10 +18,13 @@ export default function ManageDevice() {
 
   const { fido2Credentials, fido2CreateCredential } = usePasswordless();
   const createCredential = async () => {
+    if (!window.confirm("新規デバイスを登録します")) return;
+
     try {
       setIsLoading(true);
       await fido2CreateCredential({ friendlyName: deviceName });
       alert("デバイス登録に成功しました.");
+      window.location.reload();
     } catch (err: unknown) {
       alert(`デバイス登録に失敗しました. ${JSON.stringify(err)}`);
     } finally {
@@ -31,7 +35,10 @@ export default function ManageDevice() {
   return (
     <>
       <div className="border-[1px] rounded mt-3 border-slate-400">
-        <div className="cursor-pointer text-lg p-2 flex" onClick={handleShowDevices}>
+        <div
+          className="cursor-pointer text-xl p-2 flex"
+          onClick={handleShowDevices}
+        >
           {!showDevices && <span>▶ </span>}
           {showDevices && <span>▼ </span>}
           <div className="font-bold flex items-center">
@@ -59,23 +66,47 @@ export default function ManageDevice() {
                 <Loading />
               </div>
             )}
-            {fido2Credentials?.map((credential) => (
-              <div key={credential.credentialId} className="flex">
-                <div>登録名: {credential.friendlyName}</div>
-                <div className="ml-3">
-                  作成日時: {credential.createdAt.toISOString()}
+            {fido2Credentials?.length === 0 && ("認証デバイスが登録されていません。下記のテキストボックスから登録お願いします。")}
+            {fido2Credentials?.map((credential: StoredCredential) => (
+              <>
+                <div key={credential.credentialId}>
+                  <div className="flex">
+                    <div>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="green"
+                        className="size-6"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                    </div>
+                    <div className="font-bold text-lg">
+                      {credential.friendlyName}
+                    </div>
+                  </div>
+                  <div>作成日時: {credential.createdAt.toISOString()}</div>
                 </div>
-              </div>
+                <hr className="mb-2" />
+              </>
             ))}
 
-            <p className="text-lg">デバイス登録</p>
             <input
               type="text"
               placeholder="デバイス名"
               className="border border-gray-300 rounded p-1 text-slate-800 bg-white"
               onChange={handleDeviceNameChange}
             />
-            <button onClick={createCredential}>登録実行</button>
+            <button
+              className="bg-blue-500 hover:bg-blue-600 text-white p-1 rounded-lg mt-3 ml-2"
+              onClick={createCredential}
+            >
+              新規登録
+            </button>
           </div>
         </div>
       </div>
