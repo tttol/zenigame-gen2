071440d1b38ee1bb68b92ede5a05d6bba35fd8fc
tttol <tkhstol.929@gmail.com>
2025-07-28 07:21:18 +0900

    add single test

--- app/component/SingleSum.test.tsx
+++ app/component/SingleSum.test.tsx
@@ -1,14 +1,14 @@
 import { Schema } from "@/amplify/data/resource";
 import { render, screen } from "@testing-library/react";
 import { beforeAll, expect, test } from "vitest";
-import DualSum from "./DualSum";
+import SingleSum from "./SingleSum";
 
 beforeAll(() => {
   process.env.NEXT_PUBLIC_USER_A = "User A";
   process.env.NEXT_PUBLIC_USER_B = "User B";
 });
 
-test("DualSum", () => {
+test("SingleSum", () => {
   const details: Schema["Detail"]["type"][] = [
     {
       id: "1",
@@ -51,17 +51,22 @@ test("DualSum", () => {
   });
 
   render(
-    <DualSum 
+    <SingleSum 
       labeledDetails={details as Schema["Detail"]["type"][]} 
       priceFormatter={priceFormatter}
     />
   );
 
-  const actualSumA =
-    screen.getByText("User A - 未払い差引額合計").nextSibling?.textContent;
-  expect(actualSumA).toBe("￥1,750");
+  const debtAmount =
+    screen.getByText("User A - 未払い額合計").nextSibling?.textContent;
+  expect(debtAmount).toBe("￥4,500");
 
-  const actualSumB =
-    screen.getByText("User B - 未払い差引額合計").nextSibling?.textContent;
-  expect(actualSumB).toBe("￥0");
+  const userAPayment = screen.getByText("User A支出: ￥1,000");
+  expect(userAPayment).toBeDefined();
+
+  const userBPayment = screen.getByText("User B支出: ￥4,500");
+  expect(userBPayment).toBeDefined();
+
+  const totalAmount = screen.getByText("総支出: ￥5,500");
+  expect(totalAmount).toBeDefined();
 });
